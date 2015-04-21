46a5067e0d4f0bf6521d4e14c04d41fdf4719b15
jesusjzp@gmail.com <jesusjzp@gmail.com>
2015-04-21 16:59:28 +0800

    Added error handling.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var formulajs = require('formulajs');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/hello', function(req, res, next) {
+  var exeStr = 'formulajs.',
+      result = {};
+  if (req.body && req.body.str) {
+    exeStr += req.body.str;
+  }
+  try {
+    result = eval(exeStr);
+  } catch (err) {
+    result = err;
+  }
+  res.send(result);
 });
 
 module.exports = router;
